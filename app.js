8abe104c81d2483ce57969af80cc09a123cc6f9f
safa-rebaei <161932668+safa-rebaei@users.noreply.github.com>
2025-08-17 02:05:37 +0100

    creztionserveur

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const http =require("http");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -16,6 +16,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -39,3 +40,6 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+const server=http.createServer(app)//snaat serveur
+server.listen(5000,()=>(console.log("app is runnig")))
+ 
